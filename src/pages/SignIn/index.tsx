add0de5d44b3c4af0ff28006ef97a47d54805cc6
Vanessa Santana <vanessah4money@gmail.com>
2021-05-03 11:55:43 -0300

    feat:  creat components Input annd button

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
@@ -10,9 +12,9 @@ const SignIn: React.FC = () => (
       <img src={logoImg} alt="Go Barber" />
       <form>
         <h1>Faça seu logon</h1>
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input name="password" icon={FiLock} type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
         <a href="forgot">Esqueci senha</a>
       </form>
       <a href="login">
