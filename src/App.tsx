db1150e91a0f71acbd4e876b76293aa0a19e15f8
Vanessa Santana <vanessah4money@gmail.com>
2021-05-31 22:47:05 -0300

    fear: add context

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import React from 'react';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/global';
+import AuthContext from './context/AuthContext';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'diego' }}>
+      <SignIn />
+    </AuthContext.Provider>
+
     <GlobalStyle />
   </>
 );
---
+++ src/context/AuthContext.ts
@@ -0,0 +1,8 @@
+import { createContext } from 'react';
+
+interface AuthContextState {
+name: string;
+}
+const AuthContext = createContext<AuthContextState>({} as AuthContextState);
+
+export default AuthContext;
